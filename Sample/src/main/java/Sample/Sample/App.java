bdb078f60088ba09f097a78452490b26ff38273b
Sneha2611 <snehaacharya26@yahoo.in>
2021-08-18 20:19:19 +0530

    Sample push

---
+++ Sample/src/main/java/Sample/Sample/App.java
@@ -0,0 +1,7 @@
+package Sample.Sample;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ Sample/src/main/java/Sample/Sample/SampleSikuli.java
@@ -0,0 +1,10 @@
+package Sample.Sample;
+
+public class SampleSikuli {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
